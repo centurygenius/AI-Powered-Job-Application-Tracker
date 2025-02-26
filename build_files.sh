b5868e4f55f0b6e685d34ffa3dfea737cb3c5d4d
samuel abiona <samuelo.abiona@gmail.com>
2025-02-26 12:14:20 +0100

    Modify build_files.sh Vercel config file

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,4 @@
-pip install -r requirements.txt
-python manage.py collectstatic
+#!/bin/bash
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
+python3 manage.py collectstatic --noinput
