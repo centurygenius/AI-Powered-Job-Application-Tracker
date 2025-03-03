1e158d3fb356a820d8343ef4a0b810da66d6c816
samuel abiona <samuelo.abiona@gmail.com>
2025-03-03 16:19:12 +0100

    Modify settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app', 'http://localhost:5173', 'http://127.0.0.1:8000']  
 
 
 # Application definition
